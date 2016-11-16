d71267595708fa43930520c502ad95df8da42fd9
sherri010 <aminshahidi.sh@gmail.com>
2016-11-16 15:14:52 -0800

    adds login

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 var app = angular.module("chirpApp",['ngRoute']);
 
 
-app.controller("ChirpController",ChirpController);
-
-
 
 app.config(function($routeProvider){
 
@@ -13,10 +10,17 @@ app.config(function($routeProvider){
       controller:"ChirpController",
       controllerAs: "chiprCntl"
     })
+    .when("/login",{
+      templateUrl:"templates/login.html",
+      controller:"loginController",
+      controllerAs: "loginCtrl"
+    })
     .otherwise({
       redirectTo: "/tweets"
     })
 });
+
+app.controller("ChirpController",ChirpController);
 
 
 function ChirpController($http){
@@ -33,3 +37,47 @@ function ChirpController($http){
    });
 
 }
+
+app.controller("loginController",function($http,$location){
+var vm=this;
+  vm.login = function(event){
+     event.preventDefault();
+
+     $http({
+       method:"POST",
+       url:"http://localhost:3000/users/sign_in",
+       data:vm.user
+     }).success(function(user){
+        // AuthService.setSession(user);
+         $location.path("/users");
+       console.log(user)
+       }).error(function(){
+        alert("Unauth");
+     })
+  }
+
+});
+// //
+// //
+// // app.service("AuthService",function($location){
+// //   this.setSession = function(user){
+// //     localStorage.setItem("current_user", JSON.stringify(user));
+// //   }
+// //
+// //   this.getToken = function(){
+// //     var current_user = JSON.parse(localStorage.getItem("current_user"));
+// //     return current_user.auth_token;
+// //   }
+// //
+// //   this.currentUser = function (){
+// //     return JSON.parse(localStorage.getItem("current_user"));
+// //   }
+// //
+// //   this.isAuthenticated = function(){
+// //     if (this.currentUser()){
+// //       return;
+// //     }else {
+// //       $location.path("/login");
+// //     }
+// //   }
+// // });
